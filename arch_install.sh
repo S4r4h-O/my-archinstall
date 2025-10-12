7d7762de979d2c8d77d20f8b1b1d27f43f7f8a29
Sarah <sarah@F680V9.0>
2025-10-11 21:33:41 -0300

    add: system_settings function

--- arch_install.sh
+++ arch_install.sh
@@ -28,7 +28,6 @@ RESET="\033[0m"
 # read locale
 
 set_keymap() {
-  local keymap=""
   printf "${GREEN}[KEYBOARD]${RESET}: Setting up keymap...\n"
 
   while true; do
@@ -188,6 +187,124 @@ partitioning_and_mounting() {
   done
 }
 
+# TODO: Add input to select countries
+select_mirrors() {
+  printf "${BLUE}[MIRRORS]${RESET}: Installing reflector...\n"
+
+  if pacman -Sy --noconfiirm reflector; then
+    printf "${GREEN}[MIRRORS]${RESET}: Reflector installed.\n"
+  else
+    printf "${RED}[MIRRORS]${RESET}: Failed to install reflector.\n"
+  fi
+  printf "${GREEN}[MIRRORS]${RESET}: Selecting mirrors...\n"
+
+  reflector --latest 10 --fastest 5 --protocol http --download-timeout 10 --save /etc/pacman.d/mirrorlist
+}
+
+# TODO: installation should not continue if this function is not successful
+install_essentials() {
+  printf "${GREEN}[CORE]${RESET}: Installing essentials...\n"
+  if pacstrap -K /mnt base linux linux-firmware sof-firmware base-devel \
+    grub efibootmgr networkmanager amd-ucode git --noconfirm; then
+    printf "${GREEN}[CORE]${RESET}: Essentials installed successfuly!\n"
+  else
+    printf "${RED}[CORE]${RESET}: Error installing essentials.\n"
+  fi
+}
+
+run_fstab() {
+  printf "${GREEN}[SYSTEM]${RESET}: Running fstab...\n"
+
+  if genfstab -U /mnt >>/mnt/etc/fstab; then
+    printf "${GREEN}[SYSTEM]${RESET}: fstab successfuly!\n"
+  else
+    printf "${RED}[SYSTEM]${RESET}: Failed to run fstab.\n"
+  fi
+}
+
+# TODO: better valitdation
+# TODO: split function
+system_setting() {
+  printf "${GREEN}[SYSTEM]${RESET}: Setting up network configs, core services and grub...\n"
+  local root_passwd=""
+  local user_passwd=""
+  local username=""
+  local hostname=""
+  local region=""
+  local city=""
+  local locale=""
+  local mount_point="/mnt"
+
+  while true; do
+    printf "${GREEN}[SYSTEM]${RESET}: Your region: \n"
+    read -r region
+    printf "${GREEN}[SYSTEM]${RESET}: Your city: \n"
+    read -r city
+
+    if [[ -z "$region" || -z "$city" ]]; then
+      printf "${RED}[SYSTEM]${RESET}: Enter city and region!\n"
+      continue
+    fi
+
+    printf "${GREEN}[SYSTEM]${RESET}: Enter system locale: \n"
+    read -r locale
+
+    if [[ -z "$locale" ]]; then
+      printf "${RED}[SYSTEM]${RESET}: Enter a locale!\n"
+      continue
+    fi
+
+    printf "${GREEN}[SYSTEM]${RESET}: Your hostname: \n"
+    read -r hostname
+
+    if [[ -z "$hostname" ]]; then
+      printf "${RED}[SYSTEM]${RESET}: Enter a hostname!\n"
+      continue
+    fi
+
+    printf "${GREEN}[SYSTEM]${RESET}: Root password: \n"
+    read -rs root_passwd
+    echo
+
+    if [[ -z "$root_passwd" ]]; then
+      printf "${RED}[SYSTEM]${RESET}: Enter a password!\n"
+      continue
+    fi
+
+    printf "${GREEN}[SYSTEM]${RESET}: Your username: \n"
+    read -r username
+    printf "${GREEN}[SYSTEM]${RESET}: Your user password: \n"
+    read -rs user_passwd
+    echo
+
+    if [[ -z "$username" || -z "$user_passwd" ]]; then
+      printf "${RED}[SYSTEM]${RESET}: Enter an username and password!\n"
+      continue
+    fi
+
+    # Execute commands inside arch-chroot
+    arch-chroot "$mount_point" /bin/bash <<EOF
+ln -sf "/usr/share/zoneinfo/${region}/${city}" /etc/localtime
+hwclock --systohc
+echo "LANG=${locale}" > /etc/locale.conf
+sed -i "s/#${locale}/${locale}/" /etc/locale.gen
+locale-gen
+echo "KEYMAP=${keymap}" > /etc/vconsole.conf
+hostnamectl hostname ${hostname}
+echo "root:${root_passwd}" | chpasswd
+useradd -m -G wheel -s /bin/bash ${username}
+echo "${username}:${user_passwd}" | chpasswd
+echo '%wheel ALL=(ALL:ALL) ALL' > /etc/sudoers.d/wheel
+chmod 440 /etc/sudoers.d/wheel
+systemctl enable NetworkManager
+grub-install --target=x86_64-efi --efi-directory=/boot/efi/ --bootloader-id=GRUB --recheck
+grub-mkconfig -o /boot/grub/grub.cfg
+EOF
+
+    break
+  done
+}
+
 main() {
   while true; do
     printf "${BOLD}${GREEN}Want to setup wireless connections?: (y/n)${RESET} "
@@ -208,7 +325,10 @@ This process requires network connection, reexecute the script if you need wifi.
   done
 
   set_keymap
-  partitioning
+  partitioning_and_mounting
+  select_mirrors
+  install_essentials
+  run_fstab
 }
 
 main
